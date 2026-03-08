4cf4097975b0b81942bef880cc0eb4a074ebd914
Jakub Tetera <jakub.tetera@gmail.com>
2026-03-08 11:07:19 +0100

    Initial commit

---
+++ src/main/java/com/czintercity/icsec_app/IcSecAppApplication.java
@@ -0,0 +1,13 @@
+package com.czintercity.icsec_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcSecAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcSecAppApplication.class, args);
+    }
+
+}
